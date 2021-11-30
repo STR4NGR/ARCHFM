766272913ea668624180bae121b8df9bf12fd89d
STR4NGR <kerrickinc@gmail.com>
2021-12-01 00:11:01 +0300

    archinstaller

--- go.sh
+++ go.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-echo "Welcome to install ArchLinux by \033[32m FM \033[0m"
-
-#Установка кастомного шрифта для консоли
-pacman -Syy terminus-font
-setfont ter-v16b
+echo "Welcome to install ArchLinux by [\033[01;32m\] FM"
