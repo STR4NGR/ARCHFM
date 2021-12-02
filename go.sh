5d24447d2d71aa04570c445f770f2fbfc41574d0
STR4NGR <kerrickinc@gmail.com>
2021-12-02 22:58:20 +0300

    add comments

--- go.sh
+++ go.sh
@@ -3,11 +3,13 @@ echo -e "Welcome to install ArchLinux by \033[32mFM \033[0m"
 _SWAP_CAP="+4G"
 _ROOT_CAP="+25G"
 
-loadkeys ru
-setfont cyr-sun16
+#Замена стандартный шрифт
+setfont latarcyreb-sun16
 
+#Установка обновление времени через интернет
 timedatectl set-ntp true
+#Установка часового пояса
 timedatectl set-timezone Europe/Moscow
-timedatectl status
 
+#Создание разделов на жестком диске
 echo -e "n\n\n\n\n${_SWAP_CAP}\nt\n82\nn\n\n\n\n${_ROOT_CAP}\nn\n\n\n\n\nw" | fdisk /dev/sda
