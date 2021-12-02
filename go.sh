c3bd698b1ab9fa0ea32dfd584e6459cf0bdcc333
STR4NGR <kerrickinc@gmail.com>
2021-12-02 22:45:41 +0300

    check fdisk

--- go.sh
+++ go.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo -e "Welcome to install ArchLinux by \033[32mFM"
+echo -e "Welcome to install ArchLinux by \033[32mFM \033[0m"
+_SWAP_CAP="+4G"
+_ROOT_CAP="+25G"
 
 loadkeys ru
 setfont cyr-sun16
@@ -8,4 +10,4 @@ timedatectl set-ntp true
 timedatectl set-timezone Europe/Moscow
 timedatectl status
 
-echo -e "n\n\n\n\n+4G\nt\n1\n82\nw" | fdisk /dev/sda
+echo -e "n\n\n\n\n${_SWAP_CAP}\nt\n82\nn\n\n\n\n${_ROOT_CAP}\nn\n\n\n\n\nw" | fdisk /dev/sda
