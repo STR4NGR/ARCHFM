b4f2c0f335b3f4619aa3c91303d374a98a7dcf85
STR4NGR <kerrickinc@gmail.com>
2021-12-02 22:03:30 +0300

    check fdisk

--- go.sh
+++ go.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 echo -e "Welcome to install ArchLinux by \033[32mFM"
+
+loadkeys ru
+setfont cyr-sun16
+
+timedatectl set-ntp true
+timedatectl set-timezone Europe/Moscow
+timedate status
+
+fdisk /dev/sda
+n
+p
