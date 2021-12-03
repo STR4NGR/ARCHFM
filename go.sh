729065688108a9457cbde93ddcd194777760e0a2
STR4NGR <kerrickinc@gmail.com>
2021-12-03 12:48:10 +0300

    finally delete all existing table

--- go.sh
+++ go.sh
@@ -13,6 +13,6 @@ timedatectl set-ntp true
 timedatectl set-timezone Europe/Moscow
 
 #Удаление всех существующих разделов
-echo -e "d\n1\nd\n2\nd\n3\nw" | fdisk /dev/sda
+echo -e "d\n 1\n d\n 2\n d\n w\n" | fdisk /dev/sda
 #Создание разделов на жестком диске
 echo -e "n\n\n\n\n${_SWAP_CAP}\nt\n82\nn\n\n\n\n${_ROOT_CAP}\nn\n\n\n\n\nw" | fdisk /dev/sda
