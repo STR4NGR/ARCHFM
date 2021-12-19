de3cdaf0591ad05469848805a1913cfe42bd1c67
STR4NGR <kerrickinc@gmail.com>
2021-12-19 19:44:37 +0300

    create fm and enable internet

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
 # Part 2: setup the Arch Linux
+useradd -m -g users -G wheel -s /bin/bash fm
+echo -e "\033[7mВведите пароль пользователя: \033[0m"
+passwd
+systemctl enable dhcpcd.service
+systemctl start dhcpcd.service   
