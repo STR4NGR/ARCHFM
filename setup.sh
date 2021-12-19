f30f1bb84c844c9c67a8fc7d292c47e7a3244458
STR4NGR <kerrickinc@gmail.com>
2021-12-19 19:46:37 +0300

    create fm user

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 useradd -m -g users -G wheel -s /bin/bash fm
 echo -e "\033[7mВведите пароль пользователя: \033[0m"
 passwd
-systemctl enable dhcpcd.service
-systemctl start dhcpcd.service   
+#systemctl enable dhcpcd.service
+#systemctl start dhcpcd.service   
