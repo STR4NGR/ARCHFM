56e27d4e489b3e8887fc976919c309e4fe46af7e
STR4NGR <kerrickinc@gmail.com>
2021-12-19 20:00:37 +0300

    pass for fm and cyr font

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 # Part 2: setup the Arch Linux
+# Установка шрифта с поддержкой русского языка
+setfont cyr-sun16
 useradd -m -g users -G wheel -s /bin/bash fm
 echo -e "\033[7mВведите пароль пользователя: \033[0m"
-passwd
+passwd fm
 #systemctl enable dhcpcd.service
 #systemctl start dhcpcd.service   
