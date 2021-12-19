e462915f8d93799a970baea84e52ab09655b8246
STR4NGR <kerrickinc@gmail.com>
2021-12-19 23:13:28 +0300

    ver 1

--- setup.sh
+++ setup.sh
@@ -1,8 +1,19 @@
 # Part 2: setup the Arch Linux
 # Установка шрифта с поддержкой русского языка
 setfont cyr-sun16
+# Создание пользователя fm
 useradd -m -g users -G wheel -s /bin/bash fm
-echo -e "\033[7mВведите пароль пользователя: \033[0m"
+# Назначение пользователю fm прав супер-пользователя
+sed -i "s/## sudoers file./%wheel ALL=(ALL) ALL/g" /etc/sudoers
+echo -e "\033[7mВведите пароль для пользователя fm: \033[0m"
 passwd fm
-#systemctl enable dhcpcd.service
-#systemctl start dhcpcd.service   
+# Установка драйверов
+echo -e "\nY\nY\nY\nY\nY" | pacman -S pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server
+# Установка графического окружения
+echo -e "\nY\nY\nY" | pacman -S xfce4 lightdm lightdm-gtk-greeter
+echo "exec startxfce4" > ~/.xinitrc
+systemctl enable lightdm
+# Установка менеджера окон
+echo "exec i3"  > ~/.xinitrc
+echo -e "\nY" | pacman -S i3
+startx
