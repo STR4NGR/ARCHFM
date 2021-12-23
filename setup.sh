3d6acbe5177e534ed61cd20d3aade5a002d5764c
STR4NGR <kerrickinc@gmail.com>
2021-12-24 01:19:14 +0300

    fix some bugs

--- setup.sh
+++ setup.sh
@@ -58,10 +58,10 @@ elif [[ "$USER" == "fm" ]]; then
     makepkg -si
     cd ..
 # Установка Файлового Менеджера
-    echo -e "\n2\nn\nY" | yay -S xfe
+    yay -S xfe
 # Установка Visual Studio Code
-    echo -e "\nn\nY" | yay -S visual-studio-code-bin
-    echo -e "\nY\nn\nY" | yay -S python-ptvsd
+    yay -S visual-studio-code-bin
+    yay -S python-ptvsd
 # Установка Anaconda
     cd ~/Downloads
     curl -OL https://repo.anaconda.com/archive/Anaconda3-2021.11-Linux-x86_64.sh
